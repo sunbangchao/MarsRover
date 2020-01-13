daf888514ff78a04c3e74450f9e886d0643e5309
SunBangchao <>
2020-01-13 20:21:27 +0800

    update test for move()

--- src/test/java/MarsRoverTest.java
+++ src/test/java/MarsRoverTest.java
@@ -4,13 +4,9 @@ import org.junit.jupiter.api.Test;
 
 public class MarsRoverTest {
 
-    private MarsRover marsRover;
+    private MarsRover marsRover = new MarsRover();
 
 
-    @BeforeAll
-    void init(){
-        marsRover = new MarsRover();
-    }
 
     @Test
     void test_init(){
@@ -18,6 +14,42 @@ public class MarsRoverTest {
         Assertions.assertEquals(0,marsRover.getLoc().getX());
         Assertions.assertEquals(1,marsRover.getLoc().getY());
         Assertions.assertEquals(Direction.E,marsRover.getDirection());
+    }
+
+    @Test
+    void test_moveE(){
+        marsRover.init(0,1,Direction.E);
+        marsRover.move();
+        Assertions.assertEquals(1,marsRover.getLoc().getX());
+        Assertions.assertEquals(1,marsRover.getLoc().getY());
+        Assertions.assertEquals(Direction.E,marsRover.getDirection());
+    }
+
+    @Test
+    void test_moveW(){
+        marsRover.init(0,1,Direction.W);
+        marsRover.move();
+        Assertions.assertEquals(-1,marsRover.getLoc().getX());
+        Assertions.assertEquals(1,marsRover.getLoc().getY());
+        Assertions.assertEquals(Direction.W,marsRover.getDirection());
+    }
+
+    @Test
+    void test_moveN(){
+        marsRover.init(0,1,Direction.N);
+        marsRover.move();
+        Assertions.assertEquals(0,marsRover.getLoc().getX());
+        Assertions.assertEquals(2,marsRover.getLoc().getY());
+        Assertions.assertEquals(Direction.N,marsRover.getDirection());
+    }
+
+    @Test
+    void test_moveS(){
+        marsRover.init(0,1,Direction.S);
+        marsRover.move();
+        Assertions.assertEquals(0,marsRover.getLoc().getX());
+        Assertions.assertEquals(0,marsRover.getLoc().getY());
+        Assertions.assertEquals(Direction.S,marsRover.getDirection());
     }
 
 
